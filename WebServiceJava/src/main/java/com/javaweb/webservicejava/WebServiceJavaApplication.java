0b5689598babfd8d1fc49df030ef7e3cfd5592b1
Giovannapls <37786977+Giovannapls@users.noreply.github.com>
2021-05-09 19:03:15 -0300

    Swag config

--- WebServiceJava/src/main/java/com/javaweb/webservicejava/WebServiceJavaApplication.java
+++ WebServiceJava/src/main/java/com/javaweb/webservicejava/WebServiceJavaApplication.java
@@ -2,9 +2,11 @@ package com.javaweb.webservicejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class WebServiceJavaApplication {
 
     public static void main(String[] args) {
