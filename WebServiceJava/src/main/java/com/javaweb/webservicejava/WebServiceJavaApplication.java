aec07e81e8709bddcd28edc69e16e3a7dd353b3d
Giovannapls <37786977+Giovannapls@users.noreply.github.com>
2021-05-09 01:35:02 -0300

    Configurations

--- WebServiceJava/src/main/java/com/javaweb/webservicejava/WebServiceJavaApplication.java
+++ WebServiceJava/src/main/java/com/javaweb/webservicejava/WebServiceJavaApplication.java
@@ -1,9 +1,8 @@
 package com.javaweb.webservicejava;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class WebServiceJavaApplication {
--- WebServiceJava/src/main/java/com/javaweb/webservicejava/controller/ClienteController.java
+++ WebServiceJava/src/main/java/com/javaweb/webservicejava/controller/ClienteController.java
@@ -2,11 +2,9 @@ package com.javaweb.webservicejava.controller;
 
 import com.javaweb.webservicejava.entity.Cliente;
 import com.javaweb.webservicejava.service.ClienteService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
